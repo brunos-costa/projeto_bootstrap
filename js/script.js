146847eb6d3924fb8288531a1eb70888f871db0f
Bruno Costa <analistabrunocosta@gmail.com>
2023-08-09 21:26:59 -0300

    Finalizado select e acrescentado validação de campo vazio em endereço

--- js/script.js
+++ js/script.js
@@ -35,14 +35,17 @@ let endereco = document.querySelector("input[name=end]")
 endereco.addEventListener("blur",function(){
     console.log(endereco.value.length)
 
-    if(endereco.value.length >= 35){
-        alert("Endereço muito grande")
-        endereco.style.border = "2px solid red"
+    if(endereco.value!=""){// é preciso colocar esse if para impedir que, ao trocar de campo com a tecla tab, apareça o alert no campo endereço infinitamente
+        if(endereco.value.length >= 35){
+            alert("Endereço muito grande")
+            endereco.style.border = "2px solid red"
+        }
+        else{
+            alert("Tudo certo com o endereco!")
+            endereco.style.border = "2px solid yellow"
+        }
     }
-    else{
-        alert("Tudo certo com o endereco!")
-        endereco.style.border = "2px solid yellow"
-    }
+    
 })
 
 let email = document.querySelector("#email")
@@ -101,5 +104,65 @@ btnEscolaridade.addEventListener("click",(evento)=>{
     //console.log(evento)
     evento.preventDefault()// Esta função irá impedir o comportamento padrão do botão
 
-    console.log(escolaridade[1])
+    //console.log(escolaridade[2])
+    for(let itens of escolaridade){
+        //console.log(itens)
+        if(itens.checked){
+            alert(`Sua escolaridade é ${itens.value}`)// isto é um template string, outra forma de concatenar.
+            //alert("Sua escolaridade é "+itens.value)
+            //let opcao = confirm("Tem certeza?")
+            //console.log(opcao)
+        }
+    }
+
+})
+
+let escolha = document.querySelector("#escolha")
+let contrato = document.querySelector("#contrato")
+
+contrato.classList.add("d-none")
+
+escolha.addEventListener("change", ()=>{
+    if(escolha.checked){
+        contrato.classList.remove("d-none")
+        contrato.classList.add("d-block")
+    }
+    else{
+        contrato.classList.remove("d-block")
+        contrato.classList.add("d-none")
+    }
+})
+
+let concordo = document.querySelector("#concordo")
+
+concordo.addEventListener("change",()=>{
+    if(concordo.checked){
+        alert("Obrigado pela escolha")
+    }
+    else{
+        alert("Poxa, muito triste sua decisão")
+        let mensagem = prompt("Diga porque você desmarcou")// cria uma caixa de texto
+    }
+})
+
+let trabalho = document.querySelector(".form-select")
+trabalho.addEventListener("change",()=>{
+    //console.log(trabalho.options)
+
+    for(let itens of trabalho){
+        if(itens.selected){
+            //console.log(itens.value)
+            if(itens.value == "RH"){
+                trabalho.style.backgroundColor = "cyan"
+            }
+            if(itens.value == "Tecnologia"){
+                trabalho.style.backgroundColor = "brown"
+            }
+            if(itens.value == "Financeiro"){
+                trabalho.style.backgroundColor = "purple"
+            }
+        }
+
+    }
+
 })
